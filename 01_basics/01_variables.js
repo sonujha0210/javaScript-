84c75b629f98b1d57d5ccab620afa56957820d51
sonujha44 <152624545+sonujha44@users.noreply.github.com>
2024-05-29 07:22:50 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144562
+let accountEmail = "sonujha@gmail.com"
+var accountPassword = "9876"
+accountCity = "mumbai"
+let account;
+console.log(accountId)
+accountEmail="heybuddy"
+accountPassword = "78745"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,account])
