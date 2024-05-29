554d6889e3aa1a6111f2e09a78ffbf17bcf21596
sonujha44 <152624545+sonujha44@users.noreply.github.com>
2024-05-29 08:00:48 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+//alert(3 + 3) // we are using nodejs not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false 
+let undefined;
+let i = null
+
+//number  => 2 to the power  53
+//bigint 
+// string =>""
+//boolean => 0,1
+//null=> standalone value 
+//undefined =>
+//symbol => unique 
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof i);
+
